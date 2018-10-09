df279a73327e5a45e5b3f0b7234998451c1f00a2
Mai Anh Đào <1510625@hcmut.edu.vn>
2018-10-09 22:59:19 +0700

    Code phần News(hình ảnh, carousel...) dưới header

--- client/src/components/features/HomePage/HomePage.js
+++ client/src/components/features/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import News from './News/News';
 
 class HomePage extends Component {
     render() {
         return (
             <div>
+                <News />
             </div>
         );
     }
