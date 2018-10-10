94d548b58eaec2ee0d6935645e5119358667d2d3
Lê Hữu Việt Anh <anhhvle@kms-technology.com>
2018-10-10 13:17:06 +0700

    m

---
+++ client/src/components/common/ProductItem/PreviewedItem/PreviewedItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Modal from './../../utils/Modal/Modal';
+
+class PreviewedItem extends Component {
+    render() {
+        return (
+            <Modal>
+                
+            </Modal>
+        );
+    }
+}
+
+export default PreviewedItem;
