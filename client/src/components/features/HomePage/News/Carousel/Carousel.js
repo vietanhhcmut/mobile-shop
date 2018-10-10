c5fb585a2b820316cadd6609abdf500731dba716
Mai Anh Đào <1510625@hcmut.edu.vn>
2018-10-10 13:31:06 +0700

    Code carousel cho phần News

--- client/src/components/features/HomePage/News/Carousel/Carousel.js
+++ client/src/components/features/HomePage/News/Carousel/Carousel.js
@@ -9,148 +9,87 @@ class Carousel extends Component {
         strike: 1,
         order: [0, 1, 2, 3]
     }
-    getOrder = (itemIndex) => {
-        const { currentSlide } = this.state
-        const { children } = this.props
-        const numItems = 4;
-        if (itemIndex - currentSlide < 0) {
-            return numItems - Math.abs(itemIndex - currentSlide)
-        }
-        return itemIndex - currentSlide;
-        
-    }
-    doSliding = (direction, index, strike, currentSlide) => {
-        let { order } = this.state;
-        if (direction === 'prev') 
-            order = order.map(el => {
-                if (el + 3 - order[currentSlide] > 3) return  3 - order[currentSlide] - (4 - el); 
-                return el +  3 - order[currentSlide];
-            });
-        else 
-            order = order.map(el => {
-                if (el - order[currentSlide] < 0) return 4 - (order[currentSlide]  - el);
-                return el - order[currentSlide];
-            });
+    doSliding = (direction, currentSlide, oldSlide, strike) => {
+        const numSlide = this.props.children.length;
+        const x = numSlide - oldSlide;
+        const order = (direction === 'next') ?
+            [...Array(oldSlide).keys()].map(_ => _ + x).concat([...Array(x).keys()])
+            :
+            [...Array(oldSlide + 1).keys()].map(_ => _ + x - 1).concat([...Array(x - 1).keys()]);
 
-            this.setState({
-                sliding: true,
-                currentSlide: index,
-                direction,
-                order
-            })
+        this.setState({
+            currentSlide,
+            sliding: true,
+            direction,
+            strike,
+            order
+        });
 
         setTimeout(() => {
-            this.setState({
-                sliding: false
-            })
-        }, 50)
+            this.setState({ sliding: false });
+        }, 10);
     }
-    handleMovePrev = () => {
-        this.setState({strike : 1});
+    handleClickPrev = () => {
+        if (!Array.isArray(this.props.children)) return;
         const { currentSlide } = this.state;
-        const { children } = this.props;
-        const numItems = 4;
-        this.doSliding('prev', currentSlide === 0 ? numItems - 1 : currentSlide - 1, 1, currentSlide);
+        const numSlide = this.props.children.length;
+        this.doSliding('prev', currentSlide === 0 ? numSlide - 1 : currentSlide - 1, currentSlide, 1);
     }
-    handleMoveNext = () => {
-        // this.setState(prevState => {
-        //     return {
-        //         currentSlide: prevState.currentSlide + 1
-        //     };
-        // });
-        this.setState({strike : 1});
+    handleClickNext = () => {
+        if (!Array.isArray(this.props.children)) return;
         const { currentSlide } = this.state;
-        const { children } = this.props;
-        const numItems = 4;
-        this.doSliding('next', currentSlide === numItems - 1 ? 0 : currentSlide + 1, 1, currentSlide);
+        const numSlide = this.props.children.length;
+        this.doSliding('next', currentSlide === numSlide - 1 ? 0 : currentSlide + 1, currentSlide, 1);
     }
-    handleMoveToSlide = (index) => () => {
+    handleClickIndicator = (index) => () => {
+        if (!Array.isArray(this.props.children)) return;
         const { currentSlide } = this.state;
-        const { children } = this.props;
-        const numItems = 4;
-
         if (index > currentSlide) {
             const strike = index - currentSlide;
-            this.setState({direction: 'next', strike});
-            this.doSliding('next', index, strike, currentSlide);
+            this.doSliding('next', index, currentSlide, strike);
         }
         else {
             const strike = currentSlide - index;
-            this.setState({direction: 'prev', strike});
-            this.doSliding('prev', index, strike, currentSlide);
+            this.doSliding('prev', index, currentSlide, strike);
         }
-
-        // let strikeNext = 1, strikePrev = 1, direction = null;
-
-        // if (index > currentSlide) {
-        //     strikeNext = index - currentSlide;
-        // }
-        // else strikeNext = numItems - currentSlide + index;
-
-        // if (currentSlide > index) {
-        //     strikePrev = currentSlide - index;
-        // }
-        // else strikePrev = currentSlide + numItems - index;
-
-        // if (strikeNext > strikePrev) {
-        //     direction = 'prev';
-        //     this.setState({ strike: strikePrev, direction });
-        // }
-        // else {
-        //     direction = 'next';
-        //     this.setState({ strike: strikeNext, direction });
-        // }
-        // console.log(strikeNext, strikePrev, direction);
-        
     }
 
     render() {
-        const { currentSlide, strike, direction, order, sliding } = this.state;
-        const slideCount = 4;
-        const slidWidth = 100.0 / slideCount;
-
-        let translate = null;
+        const { currentSlide, direction, strike, sliding, order } = this.state;
+        let { children } = this.props;
+        if (!Array.isArray(children)) children = [children];
+        const numSlide = children.length;
+        let translate = '';
         if (direction === 'next') {
-            if (!sliding) translate = 'translateX(-' + strike +  '00%)';
-            else translate = 'translateX(0%)';
+            if (sliding) translate = 'translateX(0%)';
+            else translate = 'translateX(-' + strike + '00%)';
         }
         else if (direction === 'prev') {
-            if (!sliding) translate = 'translateX(' + (-3 + strike) +  '00%)';
-            else translate = 'translatex(-' + 3 + '00%)';
+            if (sliding) translate = 'translatex(-' + (numSlide - 1) + '00%)';
+            else translate = 'translateX(' + (-(numSlide - 1) + strike) + '00%)';
         }
-        
-        // else 
-        // if (!this.state.sliding) translate = 'translateX(-' + this.state.strike +  '00%)';
-        // else if (this.state.direction === 'prev') translate = 'translateX(-' + (this.state.strike + 1) +  '00%)';
-        // else translate = 'translateX(0%)';
-
-        const style = {
+        const wrapperStyle = {
             transition: this.state.sliding ? 'none' : 'all 0.8s ease-in-out',
             transform: translate
         }
         return (
-            <div className='carousel'>
-                <div className='wrapper'  style={style}>
-                    <div className='slide' style={{ order: order[0]}}>
-                        <img src="https://demo4leotheme.com/prestashop/leo_mobile/themes/leo_mobile/assets/img/modules/leoslideshow/slide-1.jpg" alt="image1" />
-                    </div>
-                    <div className='slide' style={{ order: order[1] }}>
-                        <img src="https://demo4leotheme.com/prestashop/leo_mobile/themes/leo_mobile/assets/img/modules/leoslideshow/slide-2.jpg" alt="image2" />
-                    </div>
-                    <div className='slide' style={{ order: order[2] }}>
-                        <img src="http://thuthuatphanmem.vn/uploads/2018/04/10/hinh-anh-dep-ve-tinh-yeu-54_052634251.jpg" alt="image3" />
-                    </div>
-                    <div className='slide' style={{ order: order[3] }}>
-                        <img src="https://dantricdn.com/thumb_w/640/2018/anh-drone-1-1514880827496.jpg" alt="image3" />
-                    </div>
+            <div className='news-carousel'>
+                <div className='news-carousel__wrapper' style={wrapperStyle}>
+                    {children.map((child, index) =>
+                        <div key={'slide' + index} className='wrapper__slide' style={{ order: order[index] }}>
+                            {child}
+                        </div>
+                    )}
                 </div>
-                <i className='arrow left-arrow material-icons' onClick={this.handleMovePrev}>keyboard_arrow_left</i>
-                <i className='arrow right-arrow material-icons' onClick={this.handleMoveNext}>keyboard_arrow_right</i>
-                <div className='dots'>
+                <i className='news-carousel__arrow news-carousel__left-arrow material-icons'
+                    onClick={this.handleClickPrev}>keyboard_arrow_left</i>
+                <i className='news-carousel__arrow news-carousel__right-arrow material-icons'
+                    onClick={this.handleClickNext}>keyboard_arrow_right</i>
+                <div className='news-carousel__dots'>
                     {
-                        [...Array(slideCount)].map((el, index) => {
-                            return <span key={'dot' + index} onClick={this.handleMoveToSlide(index)}
-                                className={this.state.currentSlide === index ? 'active' : ''}></span>
+                        [...Array(numSlide)].map((el, index) => {
+                            return <span key={'dot' + index} onClick={this.handleClickIndicator(index)}
+                                className={currentSlide === index ? 'dots__active' : ''}></span>
                         })
                     }
                 </div>
--- client/src/components/features/HomePage/News/News.js
+++ client/src/components/features/HomePage/News/News.js
@@ -43,32 +43,25 @@ class News extends Component {
         ]
     }
     render() {
-        const { topNews, carousel, rightNews1, rightNews2, bottomNews1, bottomNews2 } = this.state;
-        var settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        };
-        return(
+        const { topNews, rightNews1, rightNews2, bottomNews1, bottomNews2 } = this.state;
+        return (
             <div className='home__news'>
                 <div className='news__top-news'>
                     <a className='news-img' href={topNews.link}>
                         <img src={topNews.img} alt="Top News" />
                     </a>
                 </div>
-                
+
                 <div className='news__center-news'>
                     <div className='center-news__carousel'>
                         <Carousel>
-                            <img src="http://thuthuatphanmem.vn/uploads/2018/04/10/hinh-anh-dep-ve-tinh-yeu-54_052634251.jpg" alt="image1" />
+                            <img src="https://demo4leotheme.com/prestashop/leo_mobile/themes/leo_mobile/assets/img/modules/leoslideshow/slide-1.jpg" alt="image1" />
                             <img src="https://demo4leotheme.com/prestashop/leo_mobile/themes/leo_mobile/assets/img/modules/leoslideshow/slide-2.jpg" alt="image2" />
-                            <img src="http://thuthuatphanmem.vn/uploads/2018/09/11/hinh-anh-dep-11_044127919.jpg" alt="image3" />
+                            <img src="http://thuthuatphanmem.vn/uploads/2018/04/10/hinh-anh-dep-ve-tinh-yeu-54_052634251.jpg" alt="image3" />
                             <img src="https://dantricdn.com/thumb_w/640/2018/anh-drone-1-1514880827496.jpg" alt="image3" />
                         </Carousel>
                     </div>
-                  
+
                     <div className='center-news__right-news'>
                         <div className='right-news__top'>
                             <a className='news-img' href={rightNews1.link}>
@@ -88,9 +81,11 @@ class News extends Component {
                         <div className='bottom-news'>
                             <img src={bottomNews1.img} alt="Bottom News 1" />
                             <div className='bottom-news__content'>
-                                <h2>{bottomNews1.title}</h2>
-                                <p>{bottomNews1.decription}</p>
-                                <a href={bottomNews1.link}>READ MORE</a>
+                                <div className='content__wrapper'>
+                                    <h4>{bottomNews1.title}</h4>
+                                    <p>{bottomNews1.decription}</p>
+                                    <a href={bottomNews1.link}>READ MORE</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -98,9 +93,11 @@ class News extends Component {
                         <div className='bottom-news'>
                             <img src={bottomNews2.img} alt="Bottom News 2" />
                             <div className='bottom-news__content'>
-                                <h2>{bottomNews2.title}</h2>
-                                <p>{bottomNews2.decription}</p>
-                                <a href={bottomNews2.link}>READ MORE</a>
+                                <div className='content__wrapper'>
+                                    <h4>{bottomNews2.title}</h4>
+                                    <p>{bottomNews2.decription}</p>
+                                    <a href={bottomNews2.link}>READ MORE</a>
+                                </div>
                             </div>
                         </div>
                     </div>
