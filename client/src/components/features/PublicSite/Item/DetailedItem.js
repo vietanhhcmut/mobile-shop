748a7f2b8d070f745982624b3a13d40bf5847c50
Lê Hữu Việt Anh <anhhvle@kms-technology.com>
2018-10-10 22:58:38 +0700

    test with index

--- client/src/components/features/PublicSite/Item/DetailedItem.js
+++ client/src/components/features/PublicSite/Item/DetailedItem.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import './DetailedItem.css';
 
 class DetailedItem extends Component {
+    state = {
+        num: [1, 2, 3, 4, 5, 6]
+    }
+    handleState = i => () => {
+        const num = this.state.num;
+        num[i] = num[i] + 1;
+        console.log(num);
+        this.setState({
+            num
+        });
+    }
     render() {
         return (
             <div>
-                Item
+                {
+                    this.state.num.map((n, i) => (
+                        <div key={n + i} className="item-detail" onClick={this.handleState(i)}> {n} </div>
+                    ))
+                }
             </div>
         );
     }
